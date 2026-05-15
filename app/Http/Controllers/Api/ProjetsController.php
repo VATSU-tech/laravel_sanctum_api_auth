6232136208bb6b5ca5a7b1f95970121cd9d3ca6d
VATSU-tech <vatsumalambo@gmail.com>
2026-05-15 12:28:50 +0200

    feat: ajout des méthodes de gestion des projets dans le contrôleur Projets

--- app/Http/Controllers/Api/ProjetsController.php
+++ app/Http/Controllers/Api/ProjetsController.php
@@ -8,4 +8,19 @@ use Illuminate\Http\Request;
 class ProjetsController extends Controller
 {
     //
+    public function create(Request $request){
+
+    }
+    public function delete( $id){
+
+    }
+    public function list( $id){
+
+    }
+    public function details( $id){
+
+    }
+    public function register(Request $request){
+
+    }
 }
