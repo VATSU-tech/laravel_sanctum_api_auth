330be3c0a18cab759fed331aa2b42379563b6885
VATSU-tech <vatsumalambo@gmail.com>
2026-05-15 12:17:46 +0200

    feat: ajout des méthodes d'authentification dans le contrôleur Etudiant

--- app/Http/Controllers/Api/EtudiantController.php
+++ app/Http/Controllers/Api/EtudiantController.php
@@ -8,4 +8,16 @@ use Illuminate\Http\Request;
 class EtudiantController extends Controller
 {
     //
+    public function register(Request $request){
+
+    }
+    public function login(Request $request){
+
+    }
+    public function logout(Request $request){
+
+    }
+    public function profile(Request $request){
+
+    }
 }
