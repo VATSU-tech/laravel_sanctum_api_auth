5c90826b56111bfff6a688983b282727f2a715fe
VATSU-tech <vatsumalambo@gmail.com>
2026-05-15 13:01:15 +0200

    fix: correction des routes d'authentification et de gestion des projets dans api.php

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,17 @@ use App\Http\Controllers\Api\ProjetsController;
 use App\Http\Controllers\Api\EtudiantController;
 
 
-Route::post('/register', [EtudiantController::class, 'register']);
-Route::post('/login', [EtudiantController::class, 'login']);
+Route::post('register', [EtudiantController::class, 'register']);
+Route::post('login', [EtudiantController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::post('/logout', [EtudiantController::class, 'logout']);
-    Route::get('/profile', [EtudiantController::class, 'profile']);
+    Route::post('logout', [EtudiantController::class, 'logout']);
+    Route::get('profile', [EtudiantController::class, 'profile']);
 
     Route::post('cree_projet', [ProjetsController::class, 'create']);
-    Route::delete('delete_projet/{id}', [ProjetsController::class, 'delete']);
-    Route::get('list_projet/{id}', [ProjetsController::class,'list']);
-    Route::get('details_projet/{id}', [ProjetsController::class,'details']);
+    Route::delete('delete_projet', [ProjetsController::class, 'delete']);
+    Route::get('list_projet', [ProjetsController::class,'list']);
+    Route::get('details_projet', [ProjetsController::class,'details']);
 });
 
 Route::get('/user', function (Request $request) {
