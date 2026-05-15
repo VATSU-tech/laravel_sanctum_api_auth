f36a06aa8feeb5e847a16f6fb5e60c7dbe60c1f4
VATSU-tech <vatsumalambo@gmail.com>
2026-05-15 13:00:38 +0200

    feat: ajout des routes d'authentification et de gestion des projets dans api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjetsController;
+use App\Http\Controllers\Api\EtudiantController;
+
+
+Route::post('/register', [EtudiantController::class, 'register']);
+Route::post('/login', [EtudiantController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [EtudiantController::class, 'logout']);
+    Route::get('/profile', [EtudiantController::class, 'profile']);
+
+    Route::post('cree_projet', [ProjetsController::class, 'create']);
+    Route::delete('delete_projet/{id}', [ProjetsController::class, 'delete']);
+    Route::get('list_projet/{id}', [ProjetsController::class,'list']);
+    Route::get('details_projet/{id}', [ProjetsController::class,'details']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
