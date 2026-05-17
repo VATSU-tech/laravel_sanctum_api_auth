460ad5a62f6ed2c0d78ceb82f0d667cb7c7369a5
VATSU-tech <vatsumalambo@gmail.com>
2026-05-17 02:12:58 +0200

    feat: amélioration de la méthode de connexion dans le contrôleur Etudiant avec validation et réponse JSON

--- app/Http/Controllers/Api/EtudiantController.php
+++ app/Http/Controllers/Api/EtudiantController.php
@@ -34,13 +34,45 @@ class EtudiantController extends Controller
         ], 201);
         //enregistrement dans la base de données
     }
-    public function login(Request $request){
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $etudiant = Etudiant::where('email', '=', $request->email)->first();
+
+        if ($etudiant) {
+
+            if ($etudiant && Hash::check($request->password, $etudiant->password)) {
+                $token = $etudiant->createToken('auth_token')->plainTextToken;
+                return response()->json([
+                    'status' => 1,
+                    'message' => 'Etudiant logged in successfully',
+                    'etudiant' => $etudiant,
+                    'token' => $token,
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 0,
+                    'message' => 'Invalid email or password',
+                ], 401);
+            }
+        } else {
+            return response()->json([
+                'status' => 0,
+                'message' => 'Etudiant not found',
+            ], 404);
+
+        }
+    }
+    public function logout(Request $request)
+    {
 
     }
-    public function logout(Request $request){
-
-    }
-    public function profile(Request $request){
+    public function profile(Request $request)
+    {
 
     }
 }
